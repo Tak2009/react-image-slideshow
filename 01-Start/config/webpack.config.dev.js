1acc103b909a3d6ec81ffd54b8397a7f40460831
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-02-13 11:24:10 +0000

    loader added

--- 01-Start/config/webpack.config.dev.js
+++ 01-Start/config/webpack.config.dev.js
@@ -189,15 +189,14 @@ module.exports = {
             ],
           },
 
-　　　　　　{
-              test: /\.scss$/,
-              loaders: [
-                require.resolve('style-loader'),
-                require.resolve('css-loader'),
-                require.resolve('sass-loader'),
-
-              ]
-           },
+            {
+                test: /\.scss$/,
+                loaders: [
+                    require.resolve('style-loader'),
+                    require.resolve('css-loader'),
+                    require.resolve('sass-loader')
+                ]
+            },
 
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
